75a043eb8390e65cb879c9aa6cd65d8bd20bc03d
prince-19901022 <savagelikebird@gmail.com>
2019-05-11 16:42:17 +0600

    Enables leanback fullscreen mode on landscape orientation.

--- GrapefruitVideoPlayer/app/src/main/java/com/player/video/grapefruitvideoplayer/activities/VideoPlayerActivity.java
+++ GrapefruitVideoPlayer/app/src/main/java/com/player/video/grapefruitvideoplayer/activities/VideoPlayerActivity.java
@@ -3,6 +3,7 @@ package com.player.video.grapefruitvideoplayer.activities;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -238,6 +239,28 @@ public class VideoPlayerActivity extends AppCompatActivity implements View.OnCli
         super.onStop();
         if(Build.VERSION.SDK_INT > 23){
             releasePlayer();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            //This will hide action bar and status bar and make the activity fullscreen.
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            //This will show system ui means action bar and status bar.
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            );
         }
     }
 
